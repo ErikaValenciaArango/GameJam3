f7b58698707c080a77fdaccc537e0d76d4233f76
Jhonatan David Montañez Espitia <jhonatanmontanez05@gmail.com>
2024-11-13 00:58:50 -0500

    Pruebas de Scripts

---
+++ Assets/ScriptsJ/ControllerP.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ControllerP : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float limitX;
+    [SerializeField] private float limitY;
+    private Rigidbody rb;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0 )
+        {
+            rb.velocity = Vector3.zero;
+            return;
+        }
+        float valorZ = Input.GetAxisRaw("Vertical");
+        float valorX = Input.GetAxisRaw("Horizontal");
+
+        animator.SetInteger("Horizontal", (int)valorX);
+       // transform.Translate(new Vector3(valorX * speed, 0, valorZ * speed) * Time.deltaTime);
+       rb.velocity = new Vector3 (valorX * speed, 0, valorZ * speed);
+
+    }
+}
