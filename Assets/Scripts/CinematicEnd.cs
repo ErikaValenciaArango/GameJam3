37beee59614fc89999db5975620215702cee6d94
Erika Valencia Arango <vaerika@unicauca.edu.co>
2024-11-13 22:09:47 -0500

    cinematicaF

--- Assets/Scripts/CinematicEnd.cs
+++ Assets/Scripts/CinematicEnd.cs
@@ -20,7 +20,7 @@ public class CinematicEnd : MonoBehaviour
         // Cambia a la escena del men� principal cuando termine la cinem�tica
         if (pd == director)
         {
-            SceneManager.LoadScene("Menu");
+            SceneManager.LoadScene("Nivel");
         }
     }
 
@@ -42,7 +42,7 @@ public class CinematicEnd : MonoBehaviour
         }
 
 
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("Nivel");
     }
 }
 
